649d3c1b16c4d8cea5357d7f3a808a397255f629
Christopher Sebastian <csebastian3@gmail.com>
2012-12-07 12:00:34 -0600

    less printing

--- JDeltaSync.js
+++ JDeltaSync.js
@@ -202,7 +202,7 @@ JDeltaSync.Client.prototype.getState = function(type, id, callback) {
         callback && callback(type, id, state);
     };
     if(db.contains(id)) return afterData();
-    console.log('syncClient getState auto-fetch:', id);
+    //if(typeof console !== 'undefined') console.log('syncClient getState auto-fetch:', type, id);
     this.reset(type, id);
     db.waitForData(id, afterData);
 };
